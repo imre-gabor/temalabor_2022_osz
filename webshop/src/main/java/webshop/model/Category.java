5c0464efa3286e6268483284d91e06d355c5dd16
imre-gabor <gabor@aut.bme.hu>
2022-10-27 15:55:47 +0200

    transform model classes into entities

--- webshop/src/main/java/webshop/model/Category.java
+++ webshop/src/main/java/webshop/model/Category.java
@@ -2,15 +2,27 @@ package webshop.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
     
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
     private Integer id;
     private String name;
     
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
--- webshop/src/main/java/webshop/model/Product.java
+++ webshop/src/main/java/webshop/model/Product.java
@@ -1,16 +1,28 @@
 package webshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Product {
     
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
     private Integer id;
     private String name;
     private double price;
     
+    @ManyToOne
     private Category category;       
     
 }
