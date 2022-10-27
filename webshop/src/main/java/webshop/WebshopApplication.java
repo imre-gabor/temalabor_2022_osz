66460b70cfb92f5c2b751bfb76352625803f8000
imre-gabor <gabor@aut.bme.hu>
2022-10-27 16:00:45 +0200

    add main Spring boot class

---
+++ webshop/src/main/java/webshop/WebshopApplication.java
@@ -0,0 +1,12 @@
+package webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopApplication.class, args);
+    }
+}
