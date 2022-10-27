3c6ab37c3bfdab2569659af878f48e42878d42c6
imre-gabor <gabor@aut.bme.hu>
2022-10-27 16:25:09 +0200

    add init data, and "test" DiscountServoce

--- webshop/src/main/java/webshop/WebshopApplication.java
+++ webshop/src/main/java/webshop/WebshopApplication.java
@@ -1,12 +1,34 @@
 package webshop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WebshopApplication {
+import lombok.RequiredArgsConstructor;
+import webshop.repository.CategoryRepository;
+import webshop.repository.ProductRepository;
+import webshop.service.DiscountService;
+import webshop.service.InitDbService;
 
-    public static void main(String[] args) {
+@SpringBootApplication
+@RequiredArgsConstructor
+public class WebshopApplication implements CommandLineRunner {
+    
+    private final InitDbService initDbService;
+    private final ProductRepository productRepository;
+    private final DiscountService discountService;
+    
+    public static void main(String[] args) {        
         SpringApplication.run(WebshopApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        initDbService.initSampleData();
+        discountService.discountProductsByCategoryName("cat1", 10);
+        productRepository.findAll().forEach(p -> System.out.format("%s:%f%n", p.getName(), p.getPrice()));
+    }
+    
+    
+    
 }
